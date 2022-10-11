d2ec5c7daa4795d423c9d6a77c8f2e10bc8c6701
eoweed <90293383+eoweed@users.noreply.github.com>
2022-10-11 11:30:31 -0700

    update file structure and rename analysis script

--- MechaCar_SuspensionCoil_Analysis.R
+++ MechaCar_SuspensionCoil_Analysis.R
@@ -1,20 +1,16 @@
 library(dplyr)
-MechaCar_data <- read.csv("MechaCar_mpg.csv")
+MechaCar_data <- read.csv("Data_Files/MechaCar_mpg.csv")
 
 # Linear Regression Model for mpg
 MechaCar_data
-lm(mpg ~ vehicle_length + vehicle_weight + 
-     spoiler_angle + ground_clearance +
-     AWD, MechaCar_data)
-summary(lm(mpg ~ vehicle_length + vehicle_weight + 
-             spoiler_angle + ground_clearance +
-             AWD, MechaCar_data))
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, MechaCar_data)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, MechaCar_data))
 
 
 # Summary Stats on Suspension Coils
-suspension_data <- read.csv("Suspension_Coil.csv")
+suspension_data <- read.csv("Data_Files/Suspension_Coil.csv")
 total_summary <- suspension_data %>% summarize(Mean=mean(PSI),Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
-lot_summary <- suspension_data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
+lot_summary <- suspension_data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
 
 
 # t-tests for Suspension Coils
